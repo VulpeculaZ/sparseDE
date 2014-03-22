900271ea81972d09ed7748c5ce230a237776b10b
Ziqian Zhou <ziqian-zhou@uiowa.edu>
2014-03-21 23:34:52 -0500

    batch

--- lasso-sim.sh
+++ lasso-sim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for ((i = 1; i <= 50; i++))
 do
-    /usr/bin/nohup /bin/nice -n 10 /group/statsoft/R-patched/build-MKL-seq/bin/Rscript --vanilla ./simulation/sim.2d.lasso.R $i > tmp_$i.txt 2>&1
+    /usr/bin/nohup /bin/nice -n 10 /group/statsoft/R-patched/build-MKL-seq/bin/Rscript --vanilla ./simulation/sim.2d.lasso.R $i > tmp_$i.txt 2>&1 &
 done
